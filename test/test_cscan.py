05bcd2127a8b35deda2b5201a595cf1b0917c087
Dib, Gerges <gerges.dib@pnnl.gov>
2018-05-30 10:03:47 -0700

    nothing

--- test/test_cscan.py
+++ test/test_cscan.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot
 from os.path import join
 import importlib
 
-wave = read_ultravision(join('packages', 'scipandas', 'test', 'specimen_1-probe_6-skew_180.txt'),
+wave = read_ultravision(join('packages', 'DataScan', 'test', 'specimen_1-probe_6-skew_180.txt'),
                         45, 100e6, 5750)
 wave.coords['Z'] *= utils.time_to_depth_factor(45, 5750)
 wave = DataScan(wave)
